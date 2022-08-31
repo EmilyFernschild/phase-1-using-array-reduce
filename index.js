ae17a480badea5ae9c339142c804a37cbd3a17db
Emily Fernschild <emilyfernschild@gmail.com>
2022-08-31 18:08:53 -0400

    commit tree 69094cacb23e87b8d953b39b45d8acb3aa54f35d

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((add, numbers) => {
+    return add + numbers;
+});
+
+
+
+
+// const batteries = batteryBatches.reduce(function(countObj, totalBatteries){
+//     if (totalBatteries in countObj) {
+//         countObj[totalBatteries]++;
+//       } else {
+//         countObj[totalBatteries] = 1;
+//       }
+//       return countObj;
+//     },
+// );
+ 
